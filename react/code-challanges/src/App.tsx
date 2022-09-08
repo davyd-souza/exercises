510d75ce22aa1cc4e9cd53ca0f37003bd8090b9e
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-09-08 16:07:58 -0300

    initial commit: Reinventing the wheel. Again.

---
+++ react/code-challanges/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <div className="App">
+
+      <header className="App-hader">
+        <h1>Code Challanges</h1>  
+      </header>
+    </div>
+  )
+}
+
+export default App
