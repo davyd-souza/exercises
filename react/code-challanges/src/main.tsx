d4cab83d153bdc3462d4d37de2a200aae9af9021
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-09-08 17:56:42 -0300

    feat(react > code-challanges > NotFound): create Not Found page

--- react/code-challanges/src/main.tsx
+++ react/code-challanges/src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // COMPONENT
 import App from './App'
 import ColorRenderer from './routes/ColorRenderer/ColorRenderer'
+import NotFound from './routes/NotFound/NotFound'
 
 // STYLE
 import './index.css'
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route path="color-renderer" element={<ColorRenderer />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
