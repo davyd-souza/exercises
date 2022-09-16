a97703e411fcbb93baff32c544e480e9b8ae977c
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-09-15 22:50:29 -0300

    feat(codewars): add break camel case

---
+++ codewars/breakCamelCase.js
@@ -0,0 +1,26 @@
+/*
+Breack camelCase
+https://www.codewars.com/kata/5208f99aee097e6552000148
+
+Complete the solution so that the function will break up camel casing, using a space between words.
+
+Example
+
+$ "camelCasing"  =>  "camel Casing"
+$ "identifier"   =>  "identifier"
+$ ""   
+*/
+
+
+function breakCamelCase(str) {
+	if(!str.length) return ""
+	let arr = [...str]
+	for(let i = 0; i < arr.length; i++) {
+		if(arr[i] === arr[i].toUpperCase()) {
+			arr.splice(i, 0, ' ')
+			i++
+		}
+	}
+
+	return arr.join('');
+}
