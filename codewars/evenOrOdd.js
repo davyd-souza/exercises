718dc1cbdfa4bf42a5e0a4b49b01e7e5fec9c929
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-09-21 20:22:55 -0300

    feat: add even or odd

---
+++ codewars/evenOrOdd.js
@@ -0,0 +1,10 @@
+/*
+Even or Odd
+https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+function evenOrOdd(num) {
+  return num % 2 ? 'Odd' : 'Even'
+}
