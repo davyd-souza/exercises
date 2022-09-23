8966e8c415d35b4e0561595e9beb04f37d71ed62
Davyd Souza <davyd.eduardo.souza@hotmail.com>
2022-09-23 15:20:28 -0300

    feat(codewars): add roman numbers decoder

---
+++ codewars/romanNumDecoder.js
@@ -0,0 +1,48 @@
+/*
+Roman Numerals Decoder
+https://www.codewars.com/kata/51b6249c4612257ac0000005
+
+Create a function that takes a Roman numeral as its argument and returns its value as a numeric decimal integer. You don't need to validate the form of the Roman numeral.
+
+Modern Roman numerals are written by expressing each decimal digit of the number to be encoded separately, starting with the leftmost digit and skipping any 0s. So 1990 is rendered "MCMXC" (1000 = M, 900 = CM, 90 = XC) and 2008 is rendered "MMVIII" (2000 = MM, 8 = VIII). The Roman numeral for 1666, "MDCLXVI", uses each letter in descending order.
+
+Example:
+
+$ solution('XXI'); // should return 21
+
+Help:
+
+Symbol    Value
+I          1
+V          5
+X          10
+L          50
+C          100
+D          500
+M          1,000
+*/
+
+const romanChar = {
+	"I": 1,
+	"V": 5,
+	"X": 10,
+	"L": 50,
+	"C": 100,
+	"D": 500,
+	"M": 1000
+}
+
+function romanNumDecoder(roman) {
+	let sum = 0
+
+	for(let i = 0; i < roman.length; i++) {
+		if(romanChar[roman[i]] < romanChar[roman[i + 1]] && roman[i + 1]) {
+			sum += romanChar[roman[i + 1]] - romanChar[roman[i]]
+			i++
+		} else {
+			sum += romanChar[roman[i]]
+		}
+	}
+
+	return sum
+}
